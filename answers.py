ce063c2f70e91ef04a500a19ffbdb5426658e0b2
Jane Levin <onoprienkoyana@gmail.com>
2017-03-20 23:17:35 +0300

    inwoked get_answers from separated file

--- answers.py
+++ answers.py
@@ -1,8 +1,8 @@
 def get_answer(question):
-	
-	answers={"hello":"hi!","how are you?":"good","goodbay":"see you later"}
-	return answers[question.lower()]
+    answers = {"hello": "hi!", "how are you?": "good", "goodbye": "see you later"}
+    return answers[question.lower()]
 
-print(get_answer('Hello'))	
-print(get_answer('how are you?'))	
-print(get_answer("goodbay"))
+
+print(get_answer('Hello'))
+print(get_answer('how are you?'))
+print(get_answer("goodbye"))
---
+++ answers_scrypt.py
@@ -0,0 +1,4 @@
+from answers import get_answer
+
+a = get_answer("goodbye")
+print("answer is `{}`".format(a))
